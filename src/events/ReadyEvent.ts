51666d6e0f8c130cfcaba234c2ec01b42ae1a1b7
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-13 21:05:55 +0200

    Update ReadyEvent.ts

--- src/events/ReadyEvent.ts
+++ src/events/ReadyEvent.ts
@@ -10,7 +10,7 @@ export default class ReadyEvent implements ClientEvent {
         this.client.log.info(`${this.client.shard ? `[Shard #${this.client.shard.ids}]` : ""} I'm ready to serve `
         + `${this.client.users.cache.filter(u => !u.equals(this.client.user!)).size} users on ${this.client.guilds.cache.size} guilds!`);
         const updatePresence = async (): Promise<Presence> => this.client.user!.setPresence({
-            activity: { name: `music with ${await this.client.getUsersCount()} users!`, type: "LISTENING" }
+            activity: { name: `Prefix: !`, type: "LISTENING" }
         });
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         setInterval(updatePresence, 30 * 1000); updatePresence();
